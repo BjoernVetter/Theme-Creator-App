39b589b102b9d71ba2bd6f5fcf9a1602895c014a
Bjoern Vetter <vetterbjoern@yahoo.de>
2025-03-11 10:39:42 +0100

    finish Part 2

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -9,31 +9,57 @@ const Theme = ({ theme }) => {
   const toggleDetails = () => {
     setIsExpanded(!isExpanded);
   };
-//   console.log(isExpanded);
-
-
-
+  //   console.log(isExpanded);
 
   return (
     <div>
       {/* <h1 className="color-name">{theme.name}</h1> */}
       <button className="theme-toggle" onClick={toggleDetails}>
         {theme.name}
-        <span>{isExpanded ? "▲" : "▼"}</span>
+        <span>
+          {isExpanded ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="m18 15-6-6-6 6" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="m6 9 6 6 6-6" />
+            </svg>
+          )}
+        </span>
       </button>
-      
-        {!isExpanded && (
-        <div className="color-preview-container">    
-            {theme.colors.map ((color) =>( 
-             <div          
-        key={color.value}
-        className="color-box"
-        style={{backgroundColor: color.value}}
-                />
-))}          
-   </div>
-        )}         
-    
+
+      {!isExpanded && (
+        <div className="color-preview-container">
+          {theme.colors.map((color) => (
+            <div
+              key={color.value}
+              className="color-box"
+              style={{ backgroundColor: color.value }}
+            />
+          ))}
+        </div>
+      )}
 
       {isExpanded && (
         <div>
